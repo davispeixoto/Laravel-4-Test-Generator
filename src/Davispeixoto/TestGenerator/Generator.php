e93f5ec6cbc3c0c78114a24a18d1849c5b218436
Davis L P Peixoto <davis.peixoto@gmail.com>
2014-04-16 19:55:01 -0300

    improving test and code quality with code coverage and php mess detector

--- src/Davispeixoto/TestGenerator/Generator.php
+++ src/Davispeixoto/TestGenerator/Generator.php
@@ -31,63 +31,71 @@ class Generator {
 
 		//start filesystem stuff
 		$file = new File();
-		$str_class = $file->get(__DIR__ . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR . 'class.txt');
-		$str_methods = '';
-		$str_providers = '';
-		$str_methods_data_provider_placeholder = $file->get(__DIR__ . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR . 'methodWithDataProvider.txt');
-		$str_methods_no_data_provider_placeholder = $file->get(__DIR__ . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR . 'methodWithoutDataProvider.txt');
-		$str_providers_placeholder = $file->get(__DIR__ . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR . 'provider.txt');
+		$strClassStructure = $file->get(__DIR__ . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR . 'class.txt');
+		$strMethods = '';
+		$strProviders = '';
+		$strMethodWithData = $file->get(__DIR__ . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR . 'methodWithDataProvider.txt');
+		$strMethodNoData = $file->get(__DIR__ . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR . 'methodWithoutDataProvider.txt');
+		$strMethodProviders = $file->get(__DIR__ . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR . 'provider.txt');
 		if (!$file->exists($savePath . DIRECTORY_SEPARATOR . 'data') || !$file->isDirectory($savePath . DIRECTORY_SEPARATOR . 'data')) {
             $file->makeDirectory($savePath . DIRECTORY_SEPARATOR . 'data' , 0775);
         }
 
 		//compose replacing placeholders with data
         foreach ($methods as $method) {
-            if(
-                $method->class == $className
-                &&
-                !$method->isConstructor()
-                &&
-                !$method->isDestructor()
-            ) {
+            if($this->isValidMethod($method , $className)) {
                 $params = $method->getParameters();
-                $params_array = array();
+                $paramsArray = array();
                 if (!empty($params)) {
                     foreach($params as $param) {
-                        $params_array[] = '$' . $param->name;
+                        $paramsArray[] = '$' . $param->name;
                     }
 
                     $csvFileName = $className . '.' . ucfirst($method->name);
                     $fullCsvFileName = $csvFileName . '.csv';
-                    $methodArgs = join(' , ', $params_array);
+                    $methodArgs = join(' , ', $paramsArray);
 
                     $placeholders = array('{{className}}' , '{{methodName}}' , '{{methodNameLowerCase}}' , '{{methodArgs}}');
                     $replacements = array($className , ucfirst($method->name) , $method->name , $methodArgs);
-                    $str_methods .= str_replace($placeholders, $replacements, $str_methods_data_provider_placeholder);
+                    $strMethods .= str_replace($placeholders, $replacements, $strMethodWithData);
 
                     $placeholders = array('{{className}}' , '{{methodName}}' , '{{methodNameLowerCase}}' , '{{csvFileName}}');
                     $replacements = array($className , ucfirst($method->name) , $method->name , $csvFileName);
-                    $str_providers .= str_replace($placeholders, $replacements, $str_providers_placeholder);
+                    $strProviders .= str_replace($placeholders, $replacements, $strMethodProviders);
 
                     $file->put($savePath . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . $fullCsvFileName, '');
                 } else {
                     $placeholders = array('{{className}}' , '{{methodNameLowerCase}}' , '{{methodName}}');
                     $replacements = array($className , $method->name , ucfirst($method->name));
 
-                    $str_methods .= str_replace($placeholders, $replacements, $str_methods_no_data_provider_placeholder);
+                    $strMethods .= str_replace($placeholders, $replacements, $strMethodNoData);
                 }
             }
         }
 
 		$placeholders = array('{{className}}' , '{{methods}}' , '{{providers}}');
-		$replacements = array($className , $str_methods , $str_providers);
-		$str_class = str_replace($placeholders, $replacements, $str_class);
+		$replacements = array($className , $strMethods , $strProviders);
+		$strClassStructure = str_replace($placeholders, $replacements, $strClassStructure);
 
 		$outputFile = $savePath . DIRECTORY_SEPARATOR . $className . 'Test.php';
 
-		$file->put($outputFile, $str_class);
+		$file->put($outputFile, $strClassStructure);
 		$status = $file->exists($outputFile) && ($file->size($outputFile) > 0);
 
 		return $status;
+    }
+
+    private function isValidMethod(\ReflectionMethod $method , $className) {
+        if(
+            $method->class == $className
+                &&
+            !$method->isConstructor()
+                &&
+            !$method->isDestructor()
+        ) {
+            return true;
+        } else {
+            return false;
+        }
     }
 } 
--- tests/TestGeneratorSampleClass.php
+++ tests/TestGeneratorSampleClass.php
@@ -1,5 +1,5 @@
 <?php
-class TestGeneratorSampleClass
+class TestGeneratorSampleClass extends BaseClass
 {
 	const FOO = 'FOO';
 	
